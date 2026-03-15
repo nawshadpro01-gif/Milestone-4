4e55c32977f879657bff31fd1eae39f6a0a603e1
nawshadpro01-gif <nawshadprofessional01@gmail.com>
2026-03-15 21:58:11 +0600

    2nd commit of the day

--- module 23/js/events.js
+++ module 23/js/events.js
@@ -1,1 +1,11 @@
 console.log('external file')
+
+
+// option 2
+        function makeYellow(){
+            document.body.style.backgroundColor = 'yellow' ;
+        }
+
+        function makeRed(){
+            document.body.style.backgroundColor = ' red ' ; 
+        }
